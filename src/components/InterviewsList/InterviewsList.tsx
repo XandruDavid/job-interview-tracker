e60d0bd2e20cf2b7f93cb01ea522dd99f8df8abb
XandruDavid <xandru.david@gmail.com>
2022-08-13 21:57:57 +0200

    build(style): added tailwind with postcss

--- src/components/InterviewsList/InterviewsList.tsx
+++ src/components/InterviewsList/InterviewsList.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import { useWorkspaceStore } from "../../store/store";
 
 export const InterviewsList = () => {
-    const interviews = useWorkspaceStore((state) => state.workspace.interviews);
+    const [workspaceName, interviews] = useWorkspaceStore((state) => [state.workspace.name, state.workspace.interviews]);
     const [createNewInterview, deleteInterview] = useWorkspaceStore((state) => [state.createNewInterview, state.deleteInterview]);
 
     return (
         <div>
+            <h1 className="text-3xl font-bold underline">
+                {workspaceName}
+            </h1>
             {interviews.map((interview) => (
                 <div key={interview.id}>
                     {interview.id}
